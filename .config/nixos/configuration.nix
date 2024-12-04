896361ae4d0d8443fafad78ce84d391e7e0e3e57
Conner Ohnesorge <connerohnesorge@Conners-MacBook-Air.local>
2024-12-04 04:30:16 -0600

    latest

--- .config/nixos/configuration.nix
+++ .config/nixos/configuration.nix
@@ -6,14 +6,13 @@
 
 {
   imports = [
-    # Include the results of the hardware scan.
     ./hardware-configuration.nix
   ];
 
-  # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -77,15 +76,17 @@
   # Enable touchpad support (enabled default in most desktopManager).
   services.libinput.enable = true;
   
-  programs.hyprland = {
-    enable = true;
-    withUWSM = true;
+  programs = {
+    hyprland = {
+      enable = true;
+      withUWSM = true;
+    };
   };
   
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.connerohnesorge = {
     isNormalUser = true;
-    description = "connerohnesorge";
+    description = "Conner Ohnesorge";
     extraGroups = [
       "networkmanager"
       "wheel"
@@ -147,6 +148,7 @@
     hyprwayland-scanner
     pipewire
     grimblast
+    rofi
     gpu-screen-recorder
     matugen
     brightnessctl
@@ -281,6 +283,6 @@
   #   enable = true;
   #   enableSSHSupport = true;
   # };
-  system.stateVersion = "unstable"; # Did you read the comment?
+  system.stateVersion = "24.11"; # Did you read the comment?
 
 }

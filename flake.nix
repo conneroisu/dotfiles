3c25c5f6cc11b774ed4896421b272b1593deed1f
Conner Ohnesorge <connerohnesorge@Conners-MacBook-Air.local>
2024-12-10 06:07:25 -0600

    latest

--- flake.nix
+++ flake.nix
@@ -1,56 +1,119 @@
 {
-  description = "Conner Ohnesorge's Nix Config";
+  description = "Conner Ohnesorge's NixOS Config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixos-hardware.url = "github:nixos/nixos-hardware";
-    nix-darwin.url = "github:lnl7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    homebrew-core.url = "github:Homebrew/homebrew-core";
-    homebrew-core.flake = false;
-    homebrew-cask.url = "github:Homebrew/homebrew-cask";
-    homebrew-cask.flake = false;
-    homebrew-bundle.url = "github:Homebrew/homebrew-bundle";
-    homebrew-bundle.flake = false;
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    sops-nix.url = "github:Mic92/sops-nix";
-    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
-    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nix-ld = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:Mic92/nix-ld";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     zen-browser.url = "github:conneroisu/zen-browser-flake";
     stylix.url = "github:danth/stylix";
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+    homebrew-bundle = {
+      url = "github:homebrew/homebrew-bundle";
+      flake = false;
+    };
   };
 
   outputs =
     {
+      self,
       nixpkgs,
+      nixpkgs-unstable,
+      nix-ld,
+      nixos-hardware,
+      stylix,
+      home-manager,
+      homebrew-core,
+      homebrew-cask,
+      homebrew-bundle,
       ...
-    }@inputs:
+    }:
     let
-      user = "connerohnesorge";
+      x86_64-linux = "x86_64-linux";
+      aarch64-darwin = "aarch64-darwin";
+      pkgs = import nixpkgs {
+        inherit x86_64-linux;
+        config = {
+          allowUnfree = true;
+        };
+      };
+      unstable-pkgs = import nixpkgs-unstable {
+        inherit x86_64-linux;
+        config = {
+          allowUnfree = true;
+        };
+      };
+      inherit (self) inputs;
     in
     {
       nixosConfigurations = {
-        nixos = inputs.nixpkgs.lib.nixosSystem {
-          specialArgs = inputs;
+        nixos = nixpkgs.lib.nixosSystem {
+          specialArgs = {
+            inherit
+              x86_64-linux
+              pkgs
+              unstable-pkgs
+              inputs
+              stylix
+              ;
+          };
           modules = [
-            inputs.nixos-hardware.nixosModules.dell-xps-15-9510
-            inputs.nix-ld.nixosModules.nix-ld
+            ./hosts/x86_64-nixos
+            nixos-hardware.nixosModules.dell-xps-15-9510
+            nix-ld.nixosModules.nix-ld
             { programs.nix-ld.dev.enable = true; }
             inputs.stylix.nixosModules.stylix
-            ./hosts/nixos
           ];
         };
       };
       darwinConfigurations = {
-        "Conners-MacBook-Air" = inputs.nix-darwin.lib.darwinSystem {
-          inherit (inputs.nixpkgs) system;
-          specialArgs = inputs;
+        "Conners-MacBook-Air" = nixpkgs.lib.darwinSystem {
+          specialArgs = {
+            inherit
+              aarch64-darwin
+              pkgs
+              unstable-pkgs
+              inputs
+              stylix
+              ;
+          };
           modules = [
-            inputs.home-manager.darwinModules.home-manager
-            inputs.nix-homebrew.darwinModules.nix-homebrew
-            ./hosts/darwin
+            ./hosts/aarch64-darwin
+            home-manager.darwinModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+            }
+            {
+              nix-homebrew = {
+                # Install Homebrew under the default prefix
+                enable = true;
+                # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+                enableRosetta = true;
+                user = "connerohnesorge";
+                taps = {
+                  "homebrew/homebrew-core" = inputs.homebrew-core;
+                  "homebrew/homebrew-cask" = inputs.homebrew-cask;
+                  "homebrew/homebrew-bundle" = inputs.homebrew-bundle;
+                };
+                mutableTaps = false;
+              };
+            }
           ];
         };
       };

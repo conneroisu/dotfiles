4afaa10450caad9839c5ac53f2c049d483cf556f
connerohnesorge <conneroisu@outlook.com>
2025-01-05 16:21:40 -0600

    latest

--- devenv.nix
+++ devenv.nix
@@ -5,18 +5,10 @@
   inputs,
   ...
 }: {
-  name = "pegwings";
+  name = "dotfiles";
 
   languages = {
     nix.enable = true;
-    go = {
-      enable = true;
-      package = pkgs.go;
-    };
-    python = {
-      enable = true;
-      package = pkgs.python311;
-    };
   };
 
   packages = with pkgs; [
@@ -39,58 +31,9 @@
     flyctl
     air
     wireguard-tools
-    (python311.withPackages (ps:
-      with ps; [
-        numpy
-        pandas
-        scipy
-        matplotlib
-        scikitlearn
-        torch
-        opencv4
-        torchvision
-        torchaudio
-        selenium
-        pyarrow
-        psycopg
-        mysqlclient
-        ollama
-        black
-        requests
-        uvicorn
-        flask
-        fastapi
-        django
-        gunicorn
-        pydantic
-        mypy
-        torchdiffeq
-        beautifulsoup4
-        pillow
-        gym
-        pypdf
-        pytest
-        pip
-        sympy
-      ]))
   ];
 
   scripts = {
-    generate.exec = ''
-      cd $(git rev-parse --show-toplevel)
-      go work sync
-      for dir in $(go work edit -json | jq -r '.Use[].DiskPath'); do
-          (cd "$dir" && go generate -v ./...)
-      done
-      cd -
-      templ generate
-    '';
-    tests.exec = ''go test -v -short ./...'';
-    unit-tests.exec = ''go test -v ./...'';
-    lint.exec = ''
-      golangci-lint run
-      revive -config revive.toml $(git rev-parse --show-toplevel)/...
-    '';
     latest.exec = ''
       cd $(git rev-parse --show-toplevel) && git add . && git commit -m "latest" && git push
     '';

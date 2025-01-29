6ca15a35b183954ee32244e8d5f14985f7988286
connerohnesorge <conneroisu@outlook.com>
2025-01-29 12:03:20 -0600

    added xcodes

--- devenv.nix
+++ devenv.nix
@@ -39,7 +39,7 @@
   '';
 
   # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
+  pre-commit.hooks.shellcheck.enable = true;
 
   # See full reference at https://devenv.sh/reference/options/
 }

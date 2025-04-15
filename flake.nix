d539d644609910fe01123308822675d93d1b7657
connerohnesorge <conneroisu@outlook.com>
2025-04-15 06:56:41 -0500

    latest

--- flake.nix
+++ flake.nix
@@ -122,6 +122,7 @@
       nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
       devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
     '';
+    trusted-users = ["root" "@wheel" "connerohnesorge"];
     extra-experimental-features = "nix-command flakes";
     max-jobs = 8;
   };

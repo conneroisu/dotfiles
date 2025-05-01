ec16bf9dc38b8e0a1381000168fd286d31cca6eb
conneroisu <88785126+conneroisu@users.noreply.github.com>
2025-04-30 22:51:54 -0500

    Update .config/hypr/scripts/i-screenshot.sh
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- .config/hypr/scripts/i-screenshot.sh
+++ .config/hypr/scripts/i-screenshot.sh
@@ -4,7 +4,7 @@
 ppath=~/Pictures/Screenshots/Screenshot-"$(date +%F_%T)".png
 
 # Take a screenshot of a selected region and copy it to clipboard, then save it to the defined path
-grim -g "$(slurp)" - | wl-copy && wl-paste > "$ppath" | dunstify "Screenshot of the region taken at $(date +%F_%T)" -t 1000
+grim -g "$(slurp)" - | tee >(wl-copy) > "$ppath" && dunstify "Screenshot of the region taken at $(date +%F_%T)" -t 1000
 
 # Create a notification message
 NOTE="Screenshot @ $ppath"
@@ -13,4 +13,3 @@ xdg-open "$ppath"
 # Notify using hyprctl and dunstify
 # hyprctl notify 1 10000 0 "$NOTE"
 dunstify "$NOTE" -t 10000 -A "copy, Copy to clipboard" --action="copy:wl-copy $ppath"
-

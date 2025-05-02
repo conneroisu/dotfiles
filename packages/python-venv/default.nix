7138f3b5f7db7fe43db84679594dce3ea8074cbd
connerohnesorge <conneroisu@outlook.com>
2025-05-01 19:16:50 -0500

    latest

--- packages/python-venv/default.nix
+++ packages/python-venv/default.nix
@@ -9,23 +9,13 @@ pkgs.python312.withPackages (
       matplotlib
       huggingface-hub
       scikitlearn
-      torch
-      debugpy
-      opencv4
-      torchvision
-      selenium
       pyarrow
-      psycopg
-      mysqlclient
-      ollama
       black
       requests
       mypy
-      torchdiffeq
       beautifulsoup4
       pillow
       pypdf
-      pytest
       pip
       sympy
     ]

d1a7e35b7146cef73f6bbad51c557a56c7464fa0
connerohnesorge <conneroisu@outlook.com>
2025-05-06 11:59:01 -0500

    prevent oom errors?

--- systems/x86_64-linux/xps-nixos/default.nix
+++ systems/x86_64-linux/xps-nixos/default.nix
@@ -63,8 +63,21 @@ with lib.${namespace}; {
   };
 
   systemd = {
+    # Create a separate slice for nix-daemon that is
+    # memory-managed by the userspace systemd-oomd killer
+    slices."nix-daemon".sliceConfig = {
+      ManagedOOMMemoryPressure = "kill";
+      ManagedOOMMemoryPressureLimit = "50%";
+    };
+
+    services = {
+      "nix-daemon".serviceConfig.Slice = "nix-daemon.slice";
+      # If a kernel-level OOM event does occur anyway,
+      # strongly prefer killing nix-daemon child processes
+      "nix-daemon".serviceConfig.OOMScoreAdjust = 1000;
+      NetworkManager-wait-online.wantedBy = pkgs.lib.mkForce []; # Normally ["network-online.target"]
+    };
     targets.network-online.wantedBy = pkgs.lib.mkForce []; # Normally ["multi-user.target"]
-    services.NetworkManager-wait-online.wantedBy = pkgs.lib.mkForce []; # Normally ["network-online.target"]
   };
 
   time.timeZone = "America/Chicago";

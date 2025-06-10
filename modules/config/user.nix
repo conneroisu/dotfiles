ed708be42c52f3319d95bec7c74dde7164d41648
connerohnesorge <conneroisu@outlook.com>
2025-06-10 11:40:20 -0500

    latest

--- modules/config/user.nix
+++ modules/config/user.nix
@@ -15,6 +15,47 @@ delib.module {
         home = "/Users/${username}";
       };
     };
+
+    environment = {
+      etc."nix/nix.custom.conf".text = let
+        # This function converts an attribute set to Nix configuration lines
+        settingsToConf = settings:
+          pkgs.lib.concatStringsSep "\n" (
+            pkgs.lib.mapAttrsToList (
+              name: value: "${name} = ${
+                if builtins.isBool value
+                then pkgs.lib.boolToString value
+                else if builtins.isInt value
+                then toString value
+                else if builtins.isList value
+                then pkgs.lib.concatMapStringsSep " " (x: "${toString x}") value
+                else if builtins.isString value
+                then value
+                else throw "Unsupported type for nix.conf setting ${name}"
+              }"
+            )
+            settings
+          );
+      in
+        # Apply the function to your desired settings
+        settingsToConf {
+          # Add your nix settings here, for example:
+          experimental-features = [
+            "nix-command"
+            "flakes"
+          ];
+          trusted-users = [
+            "root"
+            "@wheel"
+            "connerohnesorge"
+          ];
+          allowed-users = [
+            "root"
+            "@wheel"
+            "connerohnesorge"
+          ];
+        };
+    };
   };
 
   nixos.always = {myconfig, ...}: let
